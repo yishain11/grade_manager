738927b40c6407ed0a1e12ceaad87a6c7d3ffa70
yishain11 <yishain11@walla.co.il>
2026-07-01 14:25:51 +0300

    added status func

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-students_data = [("alice", 55), ("bob", 34)]
+students_data = [("alice", 55), ("bob", 34), ("eve", 70), ("john", 98)]
 
 
 def main():
     student_avg = 0
     passing_students = 0
-    print("hi main")
+    for student in students_data:
+        student_grade = student[1]
+        name = student[0]
+        if student_grade < 60:
+            print(f"student {name} - you failed with grade {student_grade}")
+        elif student_grade >= 60 and student_grade < 90:
+            print(f"student {name} - you passed with grade {student_grade}")
+        else:
+            print(f"student {name} - you excellent with grade {student_grade}")
 
 
 main()
